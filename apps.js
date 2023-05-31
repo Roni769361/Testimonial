8a933c3a3970b8e030da29533a53848822381901
Ovimonno Chy Roni <oceanc9361@gmail.com>
2023-05-31 10:08:18 +0600

    first commit

---
+++ apps.js
@@ -0,0 +1,21 @@
+function nav_tabs(){
+    // console.log("Click Marce")
+    document.getElementById('nurulAkher').style.display = "block";
+    document.getElementById("roniNath").style.display = 'none';
+
+}
+
+function nav_tabsRoni(){
+    document.getElementById("nurulAkher").style.display = "none";
+    document.getElementById("roniNath").style.display = 'block';
+}
+
+function getgoogleFrom(){
+    // console.log("Click Marce")
+    document.getElementById("bodyHero").style.display = "none";
+    document.getElementById("secondHero").style.display = 'block';
+}
+
+function pustMainform(){
+    document.getElementById("bodyHero").style.display = "block";
+}
